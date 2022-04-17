d401fae93c2ec649355713e9886108f785638495
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-04-16 22:52:33 -0400

    implement base track enums

--- magmatic/enums.py
+++ magmatic/enums.py
@@ -17,8 +17,13 @@ class OpCode(Enum):
     if TYPE_CHECKING:
         value: str
 
+    #: This payload provides node statistics.
     stats = 'stats'
+
+    #: This payload signals a Lavalink event.
     event = 'event'
+
+    #: This payload requests updates to a player.
     player_update = 'playerUpdate'
 
     # Aliases
@@ -35,8 +40,68 @@ class EventType(Enum):
     if TYPE_CHECKING:
         value: str
 
+    #: This event signals that a track has started playing.
     track_start = 'TrackStartEvent'
+
+    #: This event signals that a track has stopped playing.
     track_end = 'TrackEndEvent'
+
+    #: This event signals that a track has been stuck for some reason.
     track_stuck = 'TrackStuckEvent'
+
+    #: The event signals an error during track playback.
     track_exception = 'TrackExceptionEvent'
+
+    #: This event signals that the Lavalink websocket was disconnected during playback.
     websocket_closed = 'WebSocketClosedEvent'
+
+
+class LoadType(Enum):
+    """|enum|
+
+    Represents the response type of a track loading request.
+    """
+    if TYPE_CHECKING:
+        value: str
+
+    #: One single track was loaded.
+    track_loaded = 'TRACK_LOADED'
+
+    #: A playlist containing multiple tracks were loaded.
+    playlist_loaded = 'PLAYLIST_LOADED'
+
+    #: Tracks that matched the search result were loaded. This usually indicates multiple tracks.
+    search_result = 'SEARCH_RESULT'
+
+    #: No tracks were loaded given the query.
+    no_tracks = 'NO_TRACKS'
+
+    #: The track loading request failed.
+    load_failed = 'LOAD_FAILED'
+
+    # Aliases
+    track = track_loaded
+    playlist = playlist_loaded
+    search = search_result
+
+
+class Source(Enum):
+    """|enum|
+
+    Represents the source of a track, e.g. Youtube.
+    """
+    if TYPE_CHECKING:
+        value: str
+
+    #: The track was loaded from a YouTube video.
+    youtube = 'youtube'
+
+    #: The track was loaded from a YouTube Music track.
+    youtube_music = 'youtube_music'
+
+    #: The track was loaded from a SoundCloud track.
+    soundcloud = 'soundcloud'
+
+    # TODO: Upsell Spotify support when it's available.
+    #: The track was loaded from Spotify.
+    spotify = 'spotify'
---
+++ magmatic/track.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+class Track:
+    """Represents a playable track, usually retrieved by search.
+
+    See :meth:`.Node.search_track` and :meth:`.Node.search_tracks` for more information.
+    These should not be constructed manually.
+    """
